4666751bf5d9296e3c3e7b8aeeb722d8fb519d7f
K Jayatheerth <jayatheerthkulkarni2005@gmail.com>
2025-10-05 07:58:13 +0530

    Users: Add utility functions for users
    The main functions users would use would be
    Register and Login, add these functions and then
    build the argon functions in a bottom to top fashion
    
    Signed-off-by: K Jayatheerth <jayatheerthkulkarni2005@gmail.com>

---
+++ users.go
@@ -0,0 +1,55 @@
+package auth
+
+import (
+	"context"
+	"crypto/rand"
+	"encoding/base64"
+)
+
+func generate_salt(size int) (string, error) {
+	salt := make([]byte, size)
+	_, err := rand.Read(salt)
+	if err != nil {
+		return "", err
+	}
+	return base64.RawStdEncoding.EncodeToString(salt), nil
+}
+
+func Login_user(username, password string) bool {
+	if !init_check {
+		return false
+	}
+
+	var storedHash, storedSalt string
+	query := "SELECT password_hash, salt FROM users WHERE user_id = $1"
+	err := conn.QueryRow(context.Background(), query, username).Scan(&storedHash, &storedSalt)
+	if err != nil {
+		return false
+	}
+
+	if !compare_passwords(password, storedSalt, storedHash) {
+		return false
+	}
+
+	return true
+}
+
+func Register_user(username, password string) bool {
+	if !init_check {
+		return false
+	}
+
+	salt, err := generate_salt(32)
+	if err != nil {
+		return false
+	}
+
+	hash := Hash_password(password, salt)
+
+	_, err = conn.Exec(context.Background(),
+		"INSERT INTO users (user_id, password_hash, salt) VALUES ($1, $2, $3)",
+		username, hash, salt,
+	)
+	
+	return err == nil
+}
