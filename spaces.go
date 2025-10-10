ac93192d00a439311e2f2ffe261ecaca4520ac12
K Jayatheerth <jayatheerthkulkarni2005@gmail.com>
2025-10-10 19:07:42 +0530

    Spaces: Add utility functions for space management
    This marks as the end of first set of utility
    functions for space management.
    
    Signed-off-by: K Jayatheerth <jayatheerthkulkarni2005@gmail.com>

--- spaces.go
+++ spaces.go
@@ -1,2 +1,41 @@
 package auth
 
+import (
+	"context"
+	"fmt"
+)
+
+func Create_space(name string, authority int) error {
+	if !init_check {
+		return fmt.Errorf("run auth.Init() first as a function outside API calls")
+	}
+
+	_, err := conn.Exec(context.Background(),
+		"INSERT INTO spaces(space_name, authority) VALUES ($1, $2)",
+		name, authority,
+	)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func Delete_space(name string) error {
+	if !init_check {
+		return fmt.Errorf("run auth.Init() first as a function outside API calls")
+	}
+
+	_, err := conn.Exec(
+		context.Background(),
+		"DELETE FROM spaces WHERE space_name = $1",
+		name,
+	)
+	
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
